7e375179541f3219df2306d9e34989ef996d0dc9
Mike Rhodes - HES <mikerhodes-hes@Mikes-MacBook-Air.local>
2026-02-28 10:23:04 -0700

    payload details added

--- howgood_post.py
+++ howgood_post.py
@@ -4,27 +4,40 @@ secret = "hg2026_python_engineer@!"
 endpoint = "https://howgood-apply-api.howgood.workers.dev/apply"
 
 payload = {
-    "name": "",
-    "email": "",
-    "resume": "",       # URL to your resume
-    "location": "",     # e.g. "New York, NY"
-    "linkedin": "",
-    "codeLink": "",     # URL to the repo/gist containing THIS script
-    "yearsPython": 0,
-    "yearsDjango": 0,
-    "notes": "Hi! First off, I love this application step--it seems like a quick and easy way to get to know potential developers. I'm writing this email as I eat a Chipotle burrito and work on my ",
-    "repos": [""]
+    "name": "Michael Rhodes",
+    "email": "mir333@g.harvard.edu",
+    "resume": "https://drive.google.com/file/d/13QNFJFYSAu-9lbBU46lnRht6IXK2p-de/view?usp=sharing",       # URL to your resume
+    "location": "Westminster, CO",     # e.g. "New York, NY"
+    "linkedin": "https://www.linkedin.com/in/5743rhodesm/",
+    "codeLink": "https://github.com/mrhodes-hes/howgood",     # URL to the repo/gist containing THIS script
+    "yearsPython": 15,
+    "yearsDjango": 10,
+    "notes": """Hi! First off, I love this application step--it seems like a quick and easy way to get to know potential developers. 
+    I'm writing this note as I eat a Chipotle burrito and am working on one of my assignments for my corporate responsibility course, 
+    which made coming across your company's environmental impact report for said burrito feel very apropos. Throughout my career as a software developer 
+    and data scientist, I have continually looked for ways to leverage my experience help businesses and organizations become more sustainable. 
+    To this end, I would love to learn more about your organization's mission/work, and if there is the potential for us to work together. I look forward 
+    to hearing from you!
+    
+    Additionally, the repo attached to the payload was for a job board project I started many years ago and is not currently active. While it is not the most 
+    organized project that I've done, I think it shows my ability to translate business needs into solutions. Please reach to to me at mir333@g.harvard.edu so 
+    I can grant you access.""",
+    "repos": ["https://github.com/rhodestechnologies/job_board_project"]
 }
 
 body = json.dumps(payload)
 signature = hmac.new(secret.encode(), body.encode(), hashlib.sha256).hexdigest()
 
-resp = requests.post(
-    endpoint,
-    data=body,
-    headers={
-        "Content-Type": "application/json",
-        "X-HMAC-Signature": signature,
-    },
-)
+try:
+    resp = requests.post(
+        endpoint,
+        data=body,
+        headers={
+            "Content-Type": "application/json",
+            "X-HMAC-Signature": signature,
+        },
+    )
+except Exception as e:
+    print(e)
+
 print(resp.status_code, resp.json())
